2040b612e76fcd795747d0ec6e96ef00f5b362ad
MASLORIOBANINI <151157850+MASLORIOBANINI@users.noreply.github.com>
2023-12-05 16:16:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class MyProgram
+{
+    static void Main()
+    {        
+        char value = 'A';      
+        char[] array = new char[10];
+        for (int i = 0; i < array.Length; i++)
+        {
+            switch (value)
+            {               
+                case 'A':
+                    value++;
+                    break;
+                case 'E':
+                    value++;
+                    break;
+                case 'I':
+                    value++;
+                    break;
+            }
+            array[i] = value;
+            value++;
+        }
+        for (int j = 0; j < array.Length; j++)
+        {
+            Console.WriteLine("{0} = {1}", j, array[j]);
+        }
+        Console.ReadKey();
+
+    }
+}
